642dba6108eeeb71aa596334fd36a4e94355c2da
Christian McBane <christian.j.mcbane-1@ou.edu>
2019-04-11 13:44:00 -0500

    create class for PolyLine

---
+++ PolyLine.java
@@ -0,0 +1,18 @@
+import java.awt.Color;
+import java.awt.Point;
+
+public class PolyLine extends Polygon 
+{
+
+	public PolyLine(Point pointUL, Point pointUR, int thickness, Color color, boolean filled) 
+	{
+		super(color, filled);
+		location = new Point[4];
+		
+		location[0] = pointUL;
+		location[1] = pointUR;
+		location[2] = new Point((int)pointUR.getX() , (int)pointUR.getY() + thickness);
+		location[3] = new Point((int)pointUL.getX() , (int)pointUL.getY() + thickness);
+	}
+
+}
