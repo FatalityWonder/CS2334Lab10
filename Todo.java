3ff2253a2ac62d91e9cf684e6dda5cd0089b764c
FatalityWonder <47227913+FatalityWonder@users.noreply.github.com>
2019-04-16 21:21:13 -0500

    Add todo.java

---
+++ Todo.java
@@ -0,0 +1,34 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-04-16
+OU ID_______________: 113467371
+OU EMAIL____________: christian.j.mcbane-1@ou.edu
+
+Tasks:
+1. create method stubs
+2. implement DrawPanel
+3. implement DrawFrame
+4. implement RightTriangle
+5. implement IsoscelesTriangle
+6. implement Oval
+7. write tests
+
+Estimated Time (minutes):
+1. 10
+2. 40
+3. 30
+4. 10
+5. 10
+6. 10
+7. 45
+
+Actual Time (minutes):
+1. 10
+2. 15
+3. 15
+4. 10
+5. 10
+6. 15
+7. 60
+
+*/}
